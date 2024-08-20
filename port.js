db57e3c7fa5e5a4b4ce537e6fe086ccae6047c99
nik-1718y <162183700+nik-1718y@users.noreply.github.com>
2024-08-20 12:50:39 +0530

    Initial commit

---
+++ port.js
@@ -0,0 +1,8 @@
+//   <script src="https://unpkg.com/typed.js@2.1.0/dist/typed.umd.js"></script>
+var typed=new Typed(".text",{
+    strings:["Frontend Developer","Graphic Designer","Problem Solving"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
